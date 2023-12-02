ba85f9a678d044d8b99a9af577bb2f818ecb7452
Jim <email_programmer_jim@yahoo.com>
2023-12-01 21:54:47 -0600

    Add project files.

---
+++ EdiFileInterpreter.Libraries/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EdiFileInterpreter.Libraries
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ EdiFileInterpreter.WinForm.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EdiFileInterpreter.WinForm.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
