c55ebdd9d2a8ca0be145b21163db014f5dbce5b7
Jim <email_programmer_jim@yahoo.com>
2023-12-19 12:00:42 -0600

    pushing changes to new laptp.

---
+++ EdiFileInterpreter.Libraries/Models/ParsedRawFileModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdiFileInterpreter.Libraries.Models
+{
+    public class ParsedRawFileModel
+    {
+        public string HeaderRowRaw { get; set; }
+        public string[] HeaderRowParsed { get; set; } = new string[0];
+
+    }
+}
---
+++ EdiFileInterpreter.Libraries/Services/EdiStandardDiscernmentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdiFileInterpreter.Libraries.Services
+{
+    public class EdiStandardDiscernmentService
+    {
+        //determine if 
+        //UN/Edifact
+        //Tradacoms
+        //AnsiAscX12
+        //VDA
+        //UBL
+        //https://ecosio.com/en/blog/edi-file-formats-explained/
+    }
+}
